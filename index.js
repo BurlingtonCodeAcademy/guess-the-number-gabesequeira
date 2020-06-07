466af1412a63c2d1b441a200aa6d04e6b0241961
unknown <gxs11260@vsc.edu>
2020-06-07 17:45:10 -0400

    start function lacked exposition

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ let max = 100;
 let count = 0;
 let computerGuess = 0;
 let secretNumber = 0;
+let game = 0;
+let user = [];
 
 //produces a random number
 function randomNum(min, max) {
@@ -39,44 +41,69 @@ function randomNum(min, max) {
     }
   }
 
-  async function youWin() {
-  //displays win message, tells you number of tries and gives option to play another game
-    console.log(`I got it right!`);
-    console.log(`Your number was ${secretNumber}!`);
-    if (count === 1) {
-      console.log(`I guessed it in ${count} try.`)
-    } else {
-    console.log(`I guessed it in ${count} tries.`);
-    }  let playAgain = await ask (`Play again? (Y/N)`);
-    sanitize(playAgain);
-    if(ANSWERS_POS.includes(playAgain)) {
-  //if playagain = yes, program resets the game's variables and calls start again, else quits.
-      min = 0;
-      max = 100;
-      computerGuess = 50;
-      count = 0;
-      start();
-    } else {
-      console.log(`Goodbye.`);
-      process.exit()
+    start();
+
+    function sanitizeNum() {
+
+    }
+
+    async function start() {
+        console.log(`Welcome to Guess The Number! The game where you think of a number and the computer will guess what it is, or vice versa. The number is between a minimum and a maximum range.`);
+        user = await ask(`First, tell me: What is your name?`)
+        game = await ask (`Press (1) if you'd like think of the number first and let the computer guess, or if you prefer the computer to think of a number so ${user} can guess what it is, press (2).`);
+        game = +game;
+      while(game !== 1 || game !== 2) {
+        console.log(`Please press either 1 or 2 and press [enter].`)
+        game = await ask (
+          `What version of guess the number would you like to play?`
+          ); game = +game;
+        } let min = await ask(`Now what would you like for the minimum range to be (above zero)?`);
+      while(Number.isInteger(min) !== true || min <0) {
+        console.log(`Please enter a positive integer of zero or greater.`);
+        min = await ask(`What would you like to set for the minimum?`);
+      } min = +min;
+      let max = await ask(`What would you like for the maximum number to be?`);
+      while(Number.isInteger(max) || max <= min) {
+        console.log(`Please enter an integer greater than ${min}.`);
+        max = await ask(`Please select a maximum value.`);
+      } max = +max;
+if (game === 1) {
+        computerGuess = split(min, max);
+        computerGuess = Math.floor(computerGuess);
+          //asks for the secret number and converts it to integer.
+              secretNumber = await ask("What is your secret number? I won't peek, I promise...\n");
+              while (Number.isInteger(secretNumber) !== true || secretNumber < min || secretNumber > max) {
+                console.log(`Please enter a whole number greater than ${min} and less than ${max}.`);
+                secretNumber = await ask(`Now what will I be guessing?`)
+              } guess();
+            }
+  if (game === 2) {
+    console.log(`We'll get to this later.`)
+  }
+          }
+
+    async function guess() {
+    computerGuess = Math.ceil(computerGuess);
+    let answer = await ask (`Is your number... ${computerGuess}?\n>_}`);
+    count = count + 1;
+    while (answer === undefined) {
+      console.log(`Please enter yes or no.`);
+      guess();
+    }
+    sanitize(answer);
+    while (ANSWERS_POS.includes(answer)) {
+      while (secretNumber === computerGuess) {
+        youWin();
+      } console.log(`Are you sure about that?`);
+      guess();
+    } while (ANSWERS_NEG.includes(answer)) {
+      while (secretNumber !== computerGuess) {
+        higherLower();
+      } console.log(`Are you sure about that?`);
+      guess();
     }
   }
 
-    start();
-
-    async function start() {
-    //assigns a number between 0-100 to computerGuess, trimming decimal place.
-          computerGuess = split(min, max);
-          computerGuess = Math.floor(computerGuess);
-            await ask("Let's play a game where you (human) think of a number between 1 and 100 and I (the computer) will guess what it is.");
-          //asks for the secret number and converts it to integer.
-          secretNumber = await ask("What is your secret number? I won't peek, I promise...\n>_");
-          secretNumber = parseInt(secretNumber);
-          //must be a positive integer.
-          while(!(Number.isInteger(secretNumber)) && !(secretNumber > min) && !(secretNumber < max) ) {
-            secretNumber = await ask(`Please enter a number between ${min+1} and ${max}.\n>_`)
-          } guess();
-        }
 //higherLower is a recursive function that uses while loops and if statements to get the right input from user. This is one way to solve it, but I know there's a way to refactor this code to be shorter.
   async function higherLower() {
     //while secret number is higher than guess
@@ -111,36 +138,25 @@ function randomNum(min, max) {
       }
   }
 
-async function guess() { 
-  while (secretNumber > computerGuess) {
-    let answer = await ask(`Is your number... ${Math.ceil(computerGuess)}?\n>_`);
-    count = count + 1;
-    sanitize(answer);
-    computerGuess = Math.ceil(computerGuess);
-    if (ANSWERS_NEG.includes(answer)) {
-      higherLower();
-    } else {
-      console.log('Are you sure?\n>_');
-      guess();
+  async function youWin() {
+    //displays win message, tells you number of tries and gives option to play another game
+      console.log(`I got it right!`);
+      console.log(`Your number was ${secretNumber}!`);
+      if (count === 1) {
+        console.log(`I guessed it in ${count} try.`)
+      } else {
+      console.log(`I guessed it in ${count} tries.`);
+      }  let playAgain = await ask (`Play again? (Y/N)`);
+      sanitize(playAgain);
+      if(ANSWERS_POS.includes(playAgain)) {
+    //if playagain = yes, program resets the game's variables and calls start again, else quits.
+        min = 0;
+        max = 100;
+        computerGuess = 50;
+        count = 0;
+        start();
+      } else {
+        console.log(`Goodbye.`);
+        process.exit()
+      }
     }
-  } while (secretNumber < computerGuess) {
-    let answer = await ask(`Is your number... ${Math.floor(computerGuess)}?\n>_`);
-    count = count + 1;
-    sanitize(answer);
-    computerGuess = Math.floor(computerGuess);
-    if (ANSWERS_NEG.includes(answer)) {
-      higherLower();
-    } else {
-      console.log('Are you sure?');
-      guess();
-    }
-  } while (secretNumber === computerGuess) {
-    let answer = await ask(`Is your number... ${computerGuess}?\n>_`);
-    if (ANSWERS_POS.includes(answer)) {
-      youWin();
-    } else {
-      console.log(`Are you sure?`);
-      guess()
-    }
-  }
-}
